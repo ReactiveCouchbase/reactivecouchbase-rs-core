a193fee256ed993aeb7a95c38f82b2158337e301
Mathieu <mathieu.ancelin@gmail.com>
2017-02-12 14:50:26 +0100

    Cleanup with utils

--- src/main/scala/org/reactivecouchbase/scaladsl/implicits.scala
+++ src/main/scala/org/reactivecouchbase/scaladsl/implicits.scala
@@ -1,7 +1,6 @@
 package org.reactivecouchbase.scaladsl
 
 import rx.Observable
-import rx.functions.{Action0, Action1}
 
 import scala.concurrent.{Future, Promise}
 
@@ -11,17 +10,11 @@ object Implicits {
     def asFuture: Future[T] = {
       val p = Promise[T]
       obs.subscribe(
-        new Action1[T] {
-          override def call(t: T): Unit = p.trySuccess(t)
-        },
-        new Action1[Throwable] {
-          override def call(t: Throwable): Unit = p.tryFailure(t)
-        },
-        new Action0 {
-          override def call(): Unit = {
-            if (!p.isCompleted) {
-              p.tryFailure(new RuntimeException("Observable should have produced at least a value ..."))
-            }
+        RxUtils.action1(p.trySuccess(_)),
+        RxUtils.action1(p.tryFailure(_)),
+        RxUtils.action0 { () =>
+          if (!p.isCompleted) {
+            p.tryFailure(new RuntimeException("Observable should have produced at least a value ..."))
           }
         }
       )
--- src/main/scala/org/reactivecouchbase/scaladsl/utils.scala
+++ src/main/scala/org/reactivecouchbase/scaladsl/utils.scala
@@ -1,9 +1,15 @@
 package org.reactivecouchbase.scaladsl
 
-import rx.functions.Func1
+import rx.functions.{Action0, Action1, Func1}
 
 object RxUtils {
   def func1[T, R](f: T => R): Func1[T, R] = new Func1[T, R]() {
     override def call(t: T): R = f(t)
+  }
+  def action1[T](f: T => _): Action1[T] = new Action1[T] {
+    override def call(t: T): Unit = f(t)
+  }
+  def action0[T](f: () => _): Action0 = new Action0 {
+    override def call(): Unit = f()
   }
 }
